403484a261d3dfdd9ddc91f8331235849b750d68
PatricioVargasR <1722110158@utectulancingo.edu.mx>
2024-09-22 16:01:04 -0600

    Corrección

--- static/js/utils/agregarElementos.js
+++ static/js/utils/agregarElementos.js
@@ -1,15 +1,25 @@
 const $ = (id) => document.querySelector(`.${id}`);
 
-// TODO: Arreglar esto
 // Función para agregar los datos a los contenedores correspondientes
 export const agregarDatos = (datos, claseContenedor) => {
-    console.log(datos, datos.length, claseContenedor)
+    console.log(datos, claseContenedor);
+    // Verifica si el string es JSON y conviértelo en un arreglo
+    let parsedData;
+    try {
+        parsedData = JSON.parse(datos);  // Intenta convertir el string JSON a un objeto o arreglo
+    } catch (error) {
+        console.error('Error al parsear los datos:', error);
+        parsedData = [];  // Si falla, inicializa como un arreglo vacío
+    }
+
     const contenedor = $(claseContenedor);
-    console.log(contenedor)
-    if (datos && datos.length > 0) {
-        datos.forEach(dato => {
+    console.log(contenedor);
+
+    if (Array.isArray(parsedData) && parsedData.length > 0) {
+        parsedData.forEach(dato => {
             const elemento = document.createElement('p');
-            elemento.textContent = dato;
+            elemento.className = "elemento";
+            elemento.textContent = typeof dato === 'object' && dato.texto ? dato.texto : dato;
             contenedor.appendChild(elemento);
         });
     } else {
